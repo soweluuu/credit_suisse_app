f34ce13f12a779cb2ea1528a4b14b5d9f627f6cc
soweluuu <dianalaw16@gmail.com>
2021-03-23 20:21:52 +0300

    New screen added

---
+++ app/screens/AnotherScreen.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {View} from 'react-native'
+
+function AnotherScreen(props) {
+    return (
+        <View>
+            
+        </View>
+    );
+}
+
+export default AnotherScreen;
